acdf2e5133046cc60b601313c719b4855372c524
lukasjanecek <73173856+lukasjanecek@users.noreply.github.com>
2021-02-06 12:40:32 +0100

    Update BT_SLAVE.ino

--- BT_SLAVE.ino
+++ BT_SLAVE.ino
@@ -1,15 +1,13 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial bluetooth(10, 11);  //RX,TX
+SoftwareSerial bluetooth(10, 11);  //TX, RX
 int LED = 8;
-
-
  
 void setup() 
 {
-    Serial.begin(9600);     // komunikace s host zařízením
+    Serial.begin(9600);     
  
-    // Start software serial komunikaci se spojeným prvkem
+    // Start software serial komunikaci se spárovaným zařízením
     bluetooth.begin(9600); 
      
     pinMode(LED, OUTPUT);
@@ -18,47 +16,56 @@ void setup()
     Serial.println("");
     Serial.println("nastavit Both NL & CR v sériovém monitoru");
     Serial.println("Jedem");
-    Serial.println(""); 
+    Serial.println("");
     
     bluetooth.println("AT");
-    delay(500);
-    bluetooth.println("AT+DEFAULT");
-    delay(500);
+    delay(200);
     bluetooth.println("AT+ROLE0");
-    delay(500);
+    delay(200);
     bluetooth.println("AT+IMME1");
-    delay(500);
-    bluetooth.println("AT+NAME==SLAVE");
-    delay(500);
-    /*bluetooth.println("AT+LADDR");
-    delay(500);*/
-    
+    delay(200);
+    bluetooth.println("AT+NAME==SLAVE2");
+    delay(200);
+    bluetooth.println("AT+LADDR");
+    delay(200);
 }
  
 void loop() 
-{
-    int a;
+{    
+    if (bluetooth.available()<0) {
+    bluetooth.println("AT+DEFAULT");
+    bluetooth.println("AT+NAME==SLAVE2");
+    bluetooth.println("AT+IMME1");
+    }
     
-    // listen for communication from the ESP8266 and then write it to the serial monitor
+    // čeká na příchozí komunikaci a pak ji napíše do sériového monitoru
     if (bluetooth.available()) {  
     Serial.write(bluetooth.read());  
-    }
-
-    // listen for user input and send it to the ESP8266
+    if (Serial.write(bluetooth.read()) == 1){
+        digitalWrite(LED, LOW);
+        delay(5000);
+        digitalWrite(LED, HIGH);  
+        delay(5000);
+        digitalWrite(LED, LOW);
+      }
+      }
+    
+    // čeká na vstup a posílá ho do spárovaného zařízení
     if (Serial.available()) {  
     bluetooth.write(Serial.read());  
     }
-
-   /* if (bluetooth.available()>0) {
-    a = bluetooth.write(Serial.read());
-    bluetooth.print("b:");
-    bluetooth.println(bluetooth.write(Serial.read()));
+  
     
-      if (a == '1'){
+   
+    /*if (bluetooth.available()>0) {
+    bluetooth.write(Serial.read());
+    
+    bluetooth.println(bluetooth.read());
+      if (bluetooth.read() == "1"){
         digitalWrite(LED, HIGH);  
       }
-      else if (a == '2'){
+      else if (bluetooth.read() == "2"){
         digitalWrite(LED, LOW);  
       }  
-    }  */
+    }*/
 }
